949e8224de84cd421775eed824133a5176f590bf
wuzhiwang <153678349@qq.com>
2019-08-02 15:14:25 +0800

    修改

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 
 print('hello python')
-src = cv.imread('D:/openCV/images/3.jpg')
+src = cv.imread('D:/opencv-study/images/1.png')
 cv.namedWindow("input image",cv.WINDOW_AUTOSIZE)
 cv.imshow("input image",src)
 
